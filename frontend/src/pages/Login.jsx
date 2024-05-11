01cdd0c5ead6aee7a494b061b7b6f939fb7502bd
om shankar <om8003golu@gmail.com>
2024-05-11 16:36:33 +0530

    sign up page ui

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -16,7 +16,7 @@ const Login = () => {
         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1 className="text-xl font-bold leading-tight tracking-tight text-themeColor md:text-2xl dark:text-white">
-              Sign in to your account
+              Login in to your account
             </h1>
             <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
               <div>
@@ -61,7 +61,7 @@ const Login = () => {
                 disabled={loading}
                 className="w-full text-white bg-themeColor hover:bg-themeColorHover focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-themeColor dark:hover:bg-themeColorHover dark:focus:ring-primary-800 duration-150"
               >
-                {loading ? <span>Loading...</span> : "Sign in"}
+                {loading ? <span>Loading...</span> : "Login"}
               </button>
               <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                 Don’t have an account yet?{" "}
