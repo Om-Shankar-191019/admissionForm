c53c6d2843d535ca3aaf619f2ec465d477c2d30c
om shankar <om8003golu@gmail.com>
2024-05-12 05:55:13 +0530

    mongodb connected

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 8000;
 
 const start = async () => {
   try {
+    await mongoose.connect(process.env.MONGO_URI);
     app.listen(port, () => console.log(`server is running on port ${port}...`));
   } catch (error) {
     console.log(error);
